d9cc3e185e78547c001d20fd967f304326d6ad92
Maria <maffulia@gmail.com>
2023-01-08 18:00:43 +0300

    Добавлена блок-схема к задаче 1

--- Work5/Program.cs
+++ Work5/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число:");
+int number=Convert.ToInt32(Console.ReadLine());
+int amount = number.ToString().Length;
+
+ if (amount < 3 | amount > 3)
+ {  
+    Console.WriteLine("Это не трехзначное число");
+
+ }
+ else 
+ {
+    Console.WriteLine(InCenter (number));
+ }
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int InCenter(int a)
+{
+    
+    int result = ((a / 10) % 10);
+    return result;
+}
